ff5111c3935f101041b6bbf79db5ed40637d5fac
Hhro <kwonsw212@gmail.com>
2020-11-12 01:59:10 +0900

    [ADD]FindFIle, CreateFile, and some Cache Methods

--- backend/fs.hpp
+++ backend/fs.hpp
@@ -1,17 +1,20 @@
-#include <sys/stat.h>
+#ifndef BLT_FS_HPP_
+#define BLT_FS_HPP_
 
 #include <fstream>
-#include <iostream>
 #include <map>
 #include <queue>
 #include <string>
 
-const int kMagicSize = 4;
 const int kNameSize = 8;
 const int kLengthSize = 2;
 const int kFileHeaderSize = kNameSize + kLengthSize;
-
+const int kFileSize = 256;
 const int kCacheSize = 4;
+
+const int kMaxLevel = 3;
+
+const int kFileNotFound = -1;
 
 struct FileHeader {
   std::string name;
@@ -23,36 +26,6 @@ struct FileHeader {
 struct CacheEntry {
   FileHeader fheader;
   unsigned int offset;
-};
-
-class FileSystemManager {
- private:
-  std::fstream fs_stream_;
-  std::map<std::string, int> ref_count_;
-  std::map<std::string, FileHeader> slot_;
-
-  int FindFile(const std::string& fname);
-  bool MountBLPfs(const std::string& fs);
-  inline bool Rewind() {
-    fs_stream_.seekg(0, std::ios_base::beg);
-    fs_stream_.seekp(0, std::ios_base::beg);
-  }
-
-  bool Ref(const std::string& key);
-  bool Hit(const std::string& key) { return slot_.find(key) != slot_.end(); }
-  bool Put(const std::string& key, const FileHeader& entry);
-  bool Get(const std::string& key);
-
- public:
-  FileSystemManager(const std::string& fs);
-
-  int CreateFile(const std::string& fname, const uint8_t level);
-  int ReadFile(const std::string& fname, const uint32_t pos,
-               const uint32_t len);
-  int WriteFile(const std::string& fname, const uint32_t pos,
-                const uint32_t len, const char& data);
-  bool RemoveFile(const std::string& fname);
-  int SizeFile(const std::string& fname);
 };
 
 class File {
@@ -70,3 +43,64 @@ class File {
   inline const bool in_use() const { return fheader_.in_use; }
   inline const char* data() const { return data_; }
 };
+
+class Cache {
+ private:
+  typedef std::map<std::string, CacheEntry> Slot;
+  Slot slots_;
+
+  std::string threshold_;
+
+  // methods
+
+ public:
+  // getter
+  const std::string& threshold() const { return threshold_; }
+  inline const size_t Size() const { return slots_.size(); }
+
+  // setter
+  inline void set_threshold(const std::string& threshold) {
+    threshold_.assign(threshold);
+  }
+
+  // iterators
+  Slot::iterator begin() { return slots_.begin(); }
+  Slot::iterator end() { return slots_.end(); }
+
+  // get_item
+  inline const CacheEntry& operator[](const std::string key) {
+    return slots_[key];
+  }
+
+  // methods
+  bool Hit(const std::string& key) { return slots_.find(key) != slots_.end(); }
+  void Put(const std::string& key, const CacheEntry& entry);
+  bool Get(const std::string& key, CacheEntry& entry);
+};
+
+class FileSystemManager {
+ private:
+  std::fstream fs_stream_;
+  std::map<std::string, int> ref_count_;
+  Cache cache_;
+
+  int FindFile(const std::string& fname);
+  void Ref(const std::string& key);
+  bool ParseFileHeader(const char* raw, FileHeader& fh) const;
+  inline bool Rewind() {
+    fs_stream_.seekg(0, std::ios_base::beg);
+    fs_stream_.seekp(0, std::ios_base::beg);
+  }
+
+ public:
+  bool MountBLPfs(const std::string& fs);
+  int CreateFile(const std::string& fname, const uint8_t level);
+  int ReadFile(const std::string& fname, const uint32_t pos,
+               const uint32_t len);
+  int WriteFile(const std::string& fname, const uint32_t pos,
+                const uint32_t len, const char& data);
+  bool RemoveFile(const std::string& fname);
+  int SizeFile(const std::string& fname);
+};
+
+#endif
--- backend/main.cpp
+++ backend/main.cpp
@@ -1,8 +1,11 @@
+#include <iostream>
+
 #include "fs.hpp"
 
 int main() {
   FileSystemManager FSM;
-  if (FSM.MountBLPfs("hello")) {
-    std::cout << "YEAH";
+  if (FSM.MountBLPfs("blpfs")) {
+    FSM.CreateFile("flag.txt", 3);
+    FSM.CreateFile("flag.txt", 3);
   };
 }
