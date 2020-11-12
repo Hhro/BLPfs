3f82285a1f9b490a9b8ba8b555fa69e2556b75ba
Hhro <kwonsw212@gmail.com>
2020-11-13 02:17:45 +0900

    dododo

--- backend/main.cpp
+++ backend/main.cpp
@@ -4,8 +4,20 @@
 
 int main() {
   FileSystemManager FSM;
+  char *buf = "ABCDEFGH";
+  char *out = new char[8];
   if (FSM.MountBLPfs("blpfs")) {
-    FSM.CreateFile("flag.txt", 3);
-    FSM.CreateFile("flag.txt", 3);
+    FSM.CreateFile("flag.txt", 8, 3, 1);
+    FSM.WriteFile("flag.txt", 0, 8, buf, 1);
+    FSM.ReadFile("flag.txt", 0, 8, 3, out);
+
+    FSM.CreateFile("flag.txt", 8, 3, 1);
+    FSM.CreateFile("aaaaaaaa", 8, 3, 1);
+    FSM.CreateFile("bbbbbbbb", 8, 3, 1);
+    FSM.CreateFile("cccccccc", 8, 3, 1);
+    FSM.CreateFile("dddddddd", 8, 3, 1);
+    FSM.WriteFile("dddddddd", 0, 8, buf, 1);
+    FSM.ReadFile("dddddddd", 0, 8, 3, out);
+    std::cout << out;
   };
 }
