4b4a37e94b838fdefe080f61cdf57e53cc808bde
Hhro <kwonsw212@gmail.com>
2020-11-15 13:05:58 +0900

    [ADD]fs_gen

---
+++ backend/fs_gen.cpp
@@ -0,0 +1,18 @@
+#include <cstring>
+#include <iostream>
+
+#include "dispatcher.hpp"
+#include "fs.hpp"
+
+int main() {
+  FileSystemManager FSM;
+  char *flag = "THIS-IS-A-TEST-FLAG";
+
+  std::ofstream output("blpfs");
+
+  if (FSM.MountBLPfs("blpfs")) {
+    FSM.CreateFile("flag.txt", 0x50, 1, 1);
+    FSM.WriteFile("flag.txt", 0, 0x50, flag, 1);
+    FSM.DismountBLPfs();
+  };
+}
--- backend/main.cpp
+++ backend/main.cpp
@@ -10,9 +10,8 @@ int main() {
   setvbuf(stderr, 0LL, 2, 0LL);
 
   FileSystemManager FSM;
-  char *flag = "HELLOWORLD!";
   char *flagtxt = "flag.txt";
-  char *out = new char[32];
+  char *buf = new char[32];
   char *res = new char[32];
 
   if (FSM.MountBLPfs("blpfs")) {
@@ -26,10 +25,10 @@ int main() {
 
     FSM.WriteFile("aaaa.txt", 0x100, 0x8, flagtxt, 1);
 
-    FSM.ReadFile("flag.txt", 0, 0x20, 3, out);
+    FSM.ReadFile("flag.txt", 0, 0x20, 3, buf);
 
     FSM.CreateFile("flagflag", 0x20, 3, 3);
-    FSM.WriteFile("flagflag", 0, 0x20, out, 3);
+    FSM.WriteFile("flagflag", 0, 0x20, buf, 3);
 
     FSM.RemoveFile("flag.txt", 1);
     for (int i = 0; i < 4; i++) {
@@ -38,10 +37,10 @@ int main() {
     FSM.SizeFile("flag.txt", 1);
     FSM.RemoveFile("bbbbbbbb", 1);
 
-    FSM.ReadFile("flagflag", 0, 0x20, 3, out);
+    FSM.ReadFile("flagflag", 0, 0x20, 3, buf);
     FSM.CreateFile("flag2222", 0x20, 3, 3);
     FSM.CreateFile("leaked", 0x20, 3, 3);
-    FSM.WriteFile("leaked", 0, 0x20, out, 3);
+    FSM.WriteFile("leaked", 0, 0x20, buf, 3);
 
     FSM.ReadFile("flag.txt", 0, 0x20, 1, res);
 
