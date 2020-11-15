db4c31551097e3cdece6ee351d780ad7fdaffd66
Hhro <kwonsw212@gmail.com>
2020-11-15 18:13:25 +0900

    [ADD]Delete

--- deploy/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# bash generate random 32 character alphanumeric string (lowercase only)
-RAND=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 16 | head -n 1)
-
-fname="/tmp/blpfs_"$RAND
-
-cd /home/user
-./backend/fs_gen $fname $1
-./backend/blpfsm $fname 
